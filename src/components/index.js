aac377140a9dc48858c3032b3a2f73a72e0e12bf
MeRichard123 <rcoric03@gmail.com>
2020-04-06 14:53:06 +0100

    Deploy App to Github Pages

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Cards } from "./Cards/Cards";
+export { default as Chart } from "./Chart/Chart";
+export { default as CountryPicker } from "./CountryPicker/CountryPicker";
+
+// this compiles all the components so you can import like this
+// import { Cards, Chart, CountryPicker } from "./components";
